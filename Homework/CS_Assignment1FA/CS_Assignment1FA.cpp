05ef093d36035b886737949c195a6bd621bd5888
Felipiano15$ <quantumleap1528@gmail.com>
2025-02-13 19:22:36 -0800

    My first commit

---
+++ Homework/CS_Assignment1FA/CS_Assignment1FA.cpp
@@ -0,0 +1,16 @@
+// CSAssignment1FA.cpp Felipe and Ayaan, Feb 12th, 2025//
+#include <iostream>
+using namespace std;
+
+int main()
+{  
+	//*Main code*//
+	//*Variable Declarations*//
+	float final_temperature;
+	double C;
+	//*C for Celsius*//
+
+	cout << "Type in your temperature: ";
+	cin >> C;
+	return 0;
+}
---
+++ Homework/Notes.md
@@ -0,0 +1,6 @@
+# Felipe Notes
+## A1 Notes
+### How to compile CPP
+Do this `g++ -o filename filename.cpp`
+
+Do this to run `./filename.exe` 
