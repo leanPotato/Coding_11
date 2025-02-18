ff67be9a119e45a909efcd0f44d4565175dbb693
Felipiano15$ <quantumleap1528@gmail.com>
2025-02-17 21:30:43 -0800

    New assignment A Function

---
+++ Homework/A_Function_Def/A_Function_Def.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+double total_cost(int number_par, double price_par);
+
+int main()
+{
+    double price, bill;
+
+    int number;
+
+    cout << "Enter the number of items puchased: ";
+    cin >> number;
+    cout << "Enter the price per item $";
+    cin >> price;
+
+    bill = total_cost(number, price);
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout << number << " items at "
+    << "$" << price << " each.\n"
+    << "Final bill, including tax, is $" << bill
+    << endl;
+
+    return 0;
+}
+
+double total_cost(int number_par, double price_par)
+{
+        const double TAX_RATE = 0.05;
+        double subtotal;
+
+        subtotal = price_par * number_par;
+        return (subtotal + subtotal * TAX_RATE);
+}
