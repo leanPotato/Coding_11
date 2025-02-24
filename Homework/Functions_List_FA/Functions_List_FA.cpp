0fc23fb78175c0fb307e5b544482c2efcc95ce2c
Jiang <1515527@sd61.bc.ca>
2025-02-24 13:04:15 -0800

    New Improved

--- Homework/Functions_List_FA/Functions_List_FA.cpp
+++ Homework/Functions_List_FA/Functions_List_FA.cpp
@@ -3,9 +3,46 @@
 
 using namespace std;
 
-void Showhelp (void);
+void ShowHelp(void);
 
-int main() {
+int main() 
+{
+	short Action;
 
+	cout << "What do you want to do: ";
+	cin >> Action;
+
+	if (Action == 1)
+	{
+		cout << "You have chosen to run away.";
+		cout << endl;
+	}
+	if (Action == 2)
+	{
+		cout << "You have chosen to fight.";
+		cout << endl;
+	}
+	if (Action == 3)
+	{
+		cout << "You did wrong... You die!";
+		cout << endl;
+	}
+	if (Action == 4)
+		ShowHelp ();
+
+	return 0;
+}
+
+void ShowHelp(void)
+{
+	cout << endl;
+	cout << "1 - Run";
+	cout << endl;
+	cout << "2 - Fight";
+	cout << endl;
+	cout << "3 - Surprise Action";
+	cout << endl;
+	cout << "4 - Shows this help screen";
+	cout << endl;
 
 }
