99601cb68da63f113a624e78c1d3ac299f173918
Jiang <1515527@sd61.bc.ca>
2025-02-24 13:45:54 -0800

    New Fun

---
+++ Homework/Quadratic_Equation_FA/Quadratic_Equation_FA.cpp
@@ -0,0 +1,34 @@
+//*A fun thing - 24/02/2025*//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+
+	float A, B, C;
+
+	cout << "What is your A, B, C: ";
+	cin >> A >> B >> C;
+
+	if (A == 0) {
+
+		cout << "Can't do it, too bad." << endl;
+		return 0;
+
+	}
+
+	double Discriminant = (B * B) - (4 * A * C);
+
+	if (D > 0) {
+
+
+	}
+
+
+
+
+
+
+}
