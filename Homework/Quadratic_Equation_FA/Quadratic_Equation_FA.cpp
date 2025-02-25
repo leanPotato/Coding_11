b5e43b51543b90821f20768622a96b5f3e0db5f2
Feli <quantumleap1528@gmail.com>
2025-02-25 14:53:42 -0800

    Add files via upload

--- Homework/Quadratic_Equation_FA/Quadratic_Equation_FA.cpp
+++ Homework/Quadratic_Equation_FA/Quadratic_Equation_FA.cpp
@@ -4,10 +4,9 @@
 #include <cmath>
 using namespace std;
 
-
 int main() {
 
-	float A, B, C;
+	double A, B, C;
 
 	cout << "What is your A, B, C: ";
 	cin >> A >> B >> C;
@@ -19,16 +18,39 @@ int main() {
 
 	}
 
-	double Discriminant = (B * B) - (4 * A * C);
+	double D = (B * B) - (4 * A * C);
+	cout.precision(3);
 
 	if (D > 0) {
 
+		cout << "There are two roots: ";
+		double root1 = (-B + sqrt(D)) / (2 * A);
+		cout.precision(3);
+		double root2 = (-B - sqrt(D)) / (2 * A);
+		cout.precision(3);
+		cout << "Your two roots are: " << root1 << ", " << root2 << "." << endl;
+		return 0;
+
+	} else if (D == 0) {
+
+		cout << "There is one real root: ";
+		double root = -B / (2 * A);
+		cout.precision(3);
+		cout << "Root is real and repeated: " << root << endl;
+		return 0;
+
+	} else {
+		
+		cout << "This is a complex root: ";
+		double real = (- B / (2 * A));
+		cout.precision(3);
+		double imag = (sqrt(-D)) / (2 * A);
+		cout.precision(3);
+		cout << "Your roots are complex, so this is your real root: " << real << " plus minus " << imag << endl;
+		return 0;
 
 	}
 
-
-
-
-
+	return 0;
 
 }
