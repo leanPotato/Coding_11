d46b496fb601bbeb8f3db3a4372dab8a2b7b525b
Felipiano15$ <quantumleap1528@gmail.com>
2025-03-04 18:21:01 -0800

    Final 1

--- Projects/Project_6/Project_6_FA.cpp
+++ Projects/Project_6/Project_6_FA.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// Defining constants //
+// Defining constants
 const float PAYMENT_PER_HOUR = 16.78;
 const float SOCIAL_SECURITY_TAX = 0.06;
 const float FEDERAL_INCOME_TAX = 0.14;
@@ -13,103 +13,81 @@ const float UNION_DUES = 10;
 const float WITHHELD_DEPENDENTS = 35;
 const float OVERTIME_RATE = 1.5;
 
-// Defining functions //
-double give_Gross_Pay(double hours_Worked);
-double overtime_Payment(double gross_Pay, double hours_Worked);
-//double net_Take_Home_Pay(double)//
+// Defining functions
+double give_Gross_Pay(double hours_Worked);  					 // Calculate gross pay
+double overtime_Payment(double gross_Pay, double hours_Worked);  // Calculate overtime payment
 
 int main() {
 
-	double hours_Worked, dependents, gross_Pay, new_Social_Security_Tax, new_Federal_Tax
-	, new_State_Tax, new_Overtime_Rate, overtime_Gross_Pay, your_Dependents, take_Home_Pay;
+	// State all variables
+	double hours_Worked, dependents, gross_Pay, new_Social_Security_Tax, new_Federal_Tax,
+		   new_State_Tax, take_Home_Pay;
 
+	cout.setf(ios::fixed);      // Setting the output to fixed-point
+	cout.setf(ios::showpoint);  // Setting a decimal point must
+	cout.precision(2); 		    // Setting 2 decimal places
+	
+	// Ask user for their hours worked and amount of dependents
 	cout << "Input the number of hours you work in a week: ";
 	cin >> hours_Worked;
-	cout << "Input the amount of dependents you have: ";
+	cout << "Input the amount of dependents you have:      ";
 	cin >> dependents;
+	cout << "\n------------------------------------------------\n";
 
+	// Calculate gross pay (No overtime included yet)
 	gross_Pay = give_Gross_Pay(hours_Worked);
 
-	if (hours_Worked < 40 && dependents >= 3) {
+	// If hours worked > 40, then gross pay has overtime included
+	if (hours_Worked > 40) {
+		
+		// Updates gross pay with overtime payment
+		gross_Pay = overtime_Payment(gross_Pay, hours_Worked);
+		cout << "\nIf you worked more than 40 hours,\nyour Gross payment is:       $" << gross_Pay << endl;
 
-		cout.setf(ios::fixed);          			  // Setting the output to fixed-point
-		cout.setf(ios::showpoint);       			  // Setting a decimal point must
-		cout.precision(2);
-		cout << "If you worked less than 40 hours, your payment is: $" << gross_Pay << endl;
-		new_Social_Security_Tax = SOCIAL_SECURITY_TAX * gross_Pay;
-		cout << "Your Security tax is: $" << new_Social_Security_Tax << endl;
-		new_Federal_Tax = FEDERAL_INCOME_TAX * gross_Pay;
-		cout << "Your Federal tax is $" << new_Federal_Tax << endl;
-		new_State_Tax = STATE_INCOME_TAX * gross_Pay;
-		cout << "Your State income tax is $" << new_State_Tax << endl;
-		cout << "Your withheld dependents is " << WITHHELD_DEPENDENTS << endl;
-		take_Home_Pay = gross_Pay - new_Social_Security_Tax - new_Federal_Tax - new_State_Tax - WITHHELD_DEPENDENTS;
-		cout << "Your net take home pay is $" << take_Home_Pay;
-		return 0;
-
-	} else if (hours_Worked < 40) {
-
-		cout.setf(ios::fixed);          			  // Setting the output to fixed-point
-		cout.setf(ios::showpoint);       			  // Setting a decimal point must
-		cout.precision(2);
-		cout << "If you worked less than 40 hours, your payment is: $" << gross_Pay << endl;
-		new_Social_Security_Tax = SOCIAL_SECURITY_TAX * gross_Pay;
-		cout << "Your Security tax is: $" << new_Social_Security_Tax << endl;
-		new_Federal_Tax = FEDERAL_INCOME_TAX * gross_Pay;
-		cout << "Your Federal tax is $" << new_Federal_Tax << endl;
-		new_State_Tax = STATE_INCOME_TAX * gross_Pay;
-		cout << "Your State income tax is $" << new_State_Tax << endl;
-		take_Home_Pay = gross_Pay - new_Social_Security_Tax - new_Federal_Tax - new_State_Tax;
-		cout << "Your net take home pay is $" << take_Home_Pay;
-		return 0;
-
-	} else if (hours_Worked > 40 && dependents >= 3) {
-
-		cout.setf(ios::fixed);          			  // Setting the output to fixed-point
-		cout.setf(ios::showpoint);       			  // Setting a decimal point must
-		cout.precision(2);
-		overtime_Gross_Pay = overtime_Payment(gross_Pay, hours_Worked);
-		cout << "If you worked more than 40 hours, your payment is: $" << overtime_Gross_Pay << endl;
-		new_Social_Security_Tax = SOCIAL_SECURITY_TAX * overtime_Gross_Pay;
-		cout << "Your Security tax is: $" << new_Social_Security_Tax << endl;
-		new_Federal_Tax = FEDERAL_INCOME_TAX * overtime_Gross_Pay;
-		cout << "Your Federal tax is $" << new_Federal_Tax << endl;
-		new_State_Tax = STATE_INCOME_TAX * overtime_Gross_Pay;
-		cout << "Your State income tax is $" << new_State_Tax << endl;
-		cout << "Your withheld dependents is " << WITHHELD_DEPENDENTS << endl;
-		take_Home_Pay = overtime_Gross_Pay - new_Social_Security_Tax - new_Federal_Tax - new_State_Tax - WITHHELD_DEPENDENTS;
-		cout << "Your net take-home pay is $" << take_Home_Pay;
-		return 0;
-
-	} else {
-
-		cout.setf(ios::fixed);          			  // Setting the output to fixed-point
-		cout.setf(ios::showpoint);       			  // Setting a decimal point must
-		cout.precision(2);
-		overtime_Gross_Pay = overtime_Payment(gross_Pay, hours_Worked);
-		cout << "If you worked more than 40 hours, your payment is: $" << overtime_Gross_Pay << endl;
-		new_Social_Security_Tax = SOCIAL_SECURITY_TAX * overtime_Gross_Pay;
-		cout << "Your Security tax is: $" << new_Social_Security_Tax << endl;
-		new_Federal_Tax = FEDERAL_INCOME_TAX * overtime_Gross_Pay;
-		cout << "Your Federal tax is $" << new_Federal_Tax << endl;
-		new_State_Tax = STATE_INCOME_TAX * overtime_Gross_Pay;
-		cout << "Your State income tax is $" << new_State_Tax << endl;
-		take_Home_Pay = overtime_Gross_Pay - new_Social_Security_Tax - new_Federal_Tax - new_State_Tax;
-		cout << "Your net take-home pay is $" << take_Home_Pay;
-		return 0;
+	} else {  // If hours worked < 40, then gross has NO overtime included
+		
+		// Original gross pay is left untouched
+		cout << "\nIf you worked less than 40 hours,\nyour Gross payment is:       $" << gross_Pay << endl;
 	
 	}
 	
-	return 0;
+	// If dependents >= 3, then take home pay will include withheld dependents
+	if (dependents >= 3) {
 
+		cout << "Your Withheld dependents is: $" << WITHHELD_DEPENDENTS << endl;
+		take_Home_Pay = gross_Pay - new_Social_Security_Tax - new_Federal_Tax - new_State_Tax - UNION_DUES - WITHHELD_DEPENDENTS;
+
+	} else {  // Dependents < 3
+
+		// Otherwise, dependents will not include withheld dependents
+		take_Home_Pay = gross_Pay - new_Social_Security_Tax - new_Federal_Tax - new_State_Tax - UNION_DUES;
+
+	} 
+
+	// Calculate the rest of the taxes based on the correct gross pay that was calculated earlier
+	new_Social_Security_Tax = SOCIAL_SECURITY_TAX * gross_Pay;
+	new_Federal_Tax = FEDERAL_INCOME_TAX * gross_Pay;
+	new_State_Tax = STATE_INCOME_TAX * gross_Pay;
+
+	// Show all taxes at the end
+	cout << "Your Security tax is:        $" << new_Social_Security_Tax << endl;
+	cout << "Your Federal tax is:         $" << new_Federal_Tax << endl;
+	cout << "Your State income tax is:    $" << new_State_Tax << endl;
+	cout << "Your Union dues is:          $" << UNION_DUES << endl;
+	cout << "Your Net take home pay is:   $" << take_Home_Pay << endl;
+	
+	return 0;
+	
 }
 
+// Calculate gross pay
 double give_Gross_Pay(double hours_Worked) {
 
 	return PAYMENT_PER_HOUR * hours_Worked;
 
 }
 
+// Calculate overtime payment
 double overtime_Payment(double gross_Pay, double hours_Worked) {
 
 	return gross_Pay + ((hours_Worked * PAYMENT_PER_HOUR) * OVERTIME_RATE);
