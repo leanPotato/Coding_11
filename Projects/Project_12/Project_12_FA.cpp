827e96b9685403c6ad95c7a97aaeff9cbfce113b
Feli <quantumleap1528@gmail.com>
2025-03-11 14:42:12 -0700

    Add files via upload

--- Projects/Project_12/Project_12_FA.cpp
+++ Projects/Project_12/Project_12_FA.cpp
@@ -11,60 +11,66 @@
 #include <iostream>
 using namespace std;
 
-// Defining constants for the value of pi and the conversion for cubic feet to gallons
-const float NUMBER_OF_PI = 3.14159;  // Value of pi
-const float CUBIC_FOOT = 7.48;       // Conversion for gallons
-const float CONV_TO_FEET = 12;	     // Conversion from inches to feet
-
 // Function for volume of a cylinder
-double volume_Of_Cylinder(double radius, double height);
+void calc_Volume(double radius, double height);
 
 int main() {
 
-	double radius, height, volume;
+	double radius, height;
+
+    const float CONV_TO_FEET = 12;	     // Conversion from inches to feet
 
 	// Asking for the radius
 	cout << "What is your radius casing in inches: ";
 	cin >> radius;
 	radius /= CONV_TO_FEET;  // Converting inches to feet
 
-	// Checking if the radius is 0
-	if (radius == 0) {
-
-		cout << "Sorry, radius cannot be 0.";
-		return 0;
-
-	}
-
 	// Asking for the depth
 	cout << "What is the depth of the well in feet: ";
 	cin >> height;
-
-	// Checking if the height is 0
-	if (height == 0) {
-
-		cout << "Sorry, depth cannot be 0.";
-		return 0;
-
-	}
-
-	volume = volume_Of_Cylinder(radius, height);  // Calling the function volume_Of_Cylinder to volume
-	volume = (volume * CUBIC_FOOT);  			  // Converting cubic feet to gallons
-	cout.setf(ios::fixed);          			  // Setting the output to fixed-point
-	cout.setf(ios::showpoint);       			  // Setting a decimal point must
-	cout.precision(2);               			  // Setting precision to two decimal places
-
-	// Outputing the volume of the well in gallons
-	cout << "Your casing holds: " << volume << " gallons of water." << endl;
+    
+    calc_Volume(radius, height);
 
 	return 0;
 
 }
 
 // Function to calculate the volume of the cylinder
-double volume_Of_Cylinder(double radius, double height) {
+void calc_Volume(double radius, double height) {
+
+    double volume;
+
+    // Checking if the radius is 0
+    if (radius == 0) {
+
+        cout << "Sorry, radius cannot be 0.";
+        return;
+
+    }
+
+    // Checking if the height is 0
+    if (height == 0) {
+
+        cout << "Sorry, depth cannot be 0.";
+        return;
+
+    }
+
+    // Defining constants for the value of pi and the conversion for cubic feet to gallons
+    const float NUMBER_OF_PI = 3.14159;  // Value of pi
+    const float CUBIC_FOOT = 7.48;       // Conversion for gallons
 
 	// Formula for the volume of a cylinder: V = pi * r^2 * height
-	return ((NUMBER_OF_PI * (radius * radius)) * (height));  //Return the finished calculation
+	volume = ((NUMBER_OF_PI * (radius * radius)) * height);  //Return the finished calculation
+
+    volume = (volume * CUBIC_FOOT);  			  // Converting cubic feet to gallons
+    cout.setf(ios::fixed);          			  // Setting the output to fixed-point
+    cout.setf(ios::showpoint);       			  // Setting a decimal point must
+    cout.precision(2);               			  // Setting precision to two decimal places
+
+    // Outputing the volume of the well in gallons
+    cout << "Your casing holds: " << volume << " gallons of water." << endl;
+
+    return;
 
 }
