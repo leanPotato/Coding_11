66115089ae2ac2cad438fb18edca6f46b7a42eb2
Feli <quantumleap1528@gmail.com>
2025-04-03 14:42:38 -0700

    Add files via upload

--- Homework/Die_Roller_FA/Die_Roller.cpp
+++ Homework/Die_Roller_FA/Die_Roller.cpp
@@ -18,12 +18,34 @@ using std:: chrono::duration_cast;
 using std:: chrono::system_clock;
 using std:: chrono::microseconds;
 
+void rolls_Hundred();
+
 int main() {
 
-    srand(duration_cast<microseconds>(system_clock::now().time_since_epoch()).count());
-    int randomNumber = rand();
-    int die = (randomNumber % 6) + 1;
-    cout << "You rolled a " << die << endl;
-
+    cout << "This will run a dice roller for 1 hundred times" << endl;
+    rolls_Hundred();
     return 0;
 }
+
+void rolls_Hundred() {
+
+    short Rolls = 0;
+
+    while (Rolls != 100) {
+
+        srand(duration_cast<microseconds>(system_clock::now().time_since_epoch()).count());
+        int randomNumber = rand();
+        int die = (randomNumber % 6) + 1;
+        cout << "You rolled a " << die << endl;
+        Rolls++;
+    }
+
+    cout << "\n";
+    cout << "############################" << endl;
+    cout << "#                          #" << endl;
+    cout << "#   THANKS FOR WATCHING!   #" << endl;
+    cout << "#                          #" << endl;
+    cout << "############################" << endl << endl;
+    return;
+}
+
