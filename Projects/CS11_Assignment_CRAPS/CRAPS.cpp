1f8c77c7aa8f6f0bbf9a55793ace77253903cc93
Feli <quantumleap1528@gmail.com>
2025-04-08 15:10:15 -0700

    Add files via upload

--- Projects/CS11_Assignment_CRAPS/CRAPS.cpp
+++ Projects/CS11_Assignment_CRAPS/CRAPS.cpp
@@ -34,24 +34,47 @@ int main(void) {
 
 
     do {
+       
+        char isRepeat = 'y';
 
-        ShowInformation(Money);
+        while(isRepeat == 'y' || isRepeat == 'Y') {
 
-        Bet = GetBet();
-        BetMoney = GetAmount();
-        DiceValue = DoDiceThrow();
-        MoneyEarned = DoMoneyCalc(DiceValue, Bet, BetMoney);
+            ShowInformation(Money);
 
-        Money -= BetMoney;
+            Bet = GetBet();
+            
+            if (GetBet == 0) {
+                cout << "\n\nWould you like to repeat the program again? (y/n): ";  // Ask if program wants to be runned again
+                cin >> isRepeat;
+            }
+            else {
+                BetMoney    = GetAmount();
+                DiceValue   = DoDiceThrow();
+                MoneyEarned = DoMoneyCalc(DiceValue, Bet, BetMoney);
 
-        if (MoneyEarned == 0) {
+                Money -= BetMoney;
+            }
+            if (MoneyEarned == 0) {
 
-            cout << "You won " << MoneyEarned;
-            cout << " dollars. Number was: " << DiceValue;
-            cout << endl << endl;
+                cout << "You won " << MoneyEarned;
+                cout << " dollars. Number was: " << DiceValue;
+                cout << endl << endl;
 
-            Money += MoneyEarned;
+                Money += MoneyEarned;
+            }
+
+            // Check if isRepeat is y
+            if (isRepeat != 'y' && isRepeat != 'Y') {
+                cout << "\nThanks for watching!";  // Output thank you message
+                cout << "\n\n####################################################\n\n";
+                system("pause");  // Pause until user presses any key
+            }
+            else {
+                cout << "\n####################################################\n";  // Display a barrier for new line
+            }
+
         }
+        
     } while (Money > 100);
     cout << "Game Over. Keep $" << Money << " for the ride home";
     cout << endl;
@@ -61,30 +84,28 @@ int main(void) {
 
 void ShowIntroScreen(void) {
 
-    cout << "                   Welcome to Craps 1.0";
-    cout << endl << endl;
-    cout << "Here are the ruled:";
-    cout << endl << endl;
-
-    cout << "You have 1000 dollars to start gambling. ";
-    cout << endl << endl;
-
-    cout << " You can do three different bets. You can bet on ";
-    cout << "numbers 2 and 12 which will give ";
-    cout << "you a win ratio of ";
-    cout << "5 to 1 if you win. You can also bet on the numbers 4 ";
-    cout << "and 10 ";
-    cout << "which will give you a win ratio of 2.5 to 1. ";
     cout << endl;
-    cout << "The last kind of bet you can do is on the numbers 6 and 8 ";
-    cout << "which will give you a win ratio of 1.5 to 1. ";
+    cout << "############################" << endl;
+    cout << "#                          #" << endl;
+    cout << "#   WELCOME TO CRAPS 1.0!  #" << endl;
+    cout << "#                          #" << endl;
+    cout << "############################" << endl << endl;
+    cout << "Here are the rules:";
+    cout << endl << endl;
+
+    cout << "You have 1000 dollars to start gambling.";
+    cout << endl;
+    cout << "You can do three different bets. You can bet on ";
+    cout << "numbers 2 and 12 which will give you a win ratio of 5 to 1 if you win. " << endl;
+    cout << "You can also bet on the numbers 4 and 10 which will give you a win ratio of 2.5 to 1. " << endl;
+    cout << "The last kind of bet you can do is on the numbers 6 and 8 which will give you a win ratio of 1.5 to 1. ";
     cout << endl << endl;
 
     cout << "The minimum amount to bet is 10 dollars and the maximum 100 dollars.";
     cout << endl << endl;
 
     cout << "Have fun playing.";
-    cout << endl << endl << endl;
+    cout << endl << endl;
 }
 
 void ShowInformation(unsigned long Money) {
@@ -94,14 +115,15 @@ void ShowInformation(unsigned long Money) {
 
 short GetBet(void) {
     unsigned short BetType;
-    cout << "Enter type of bet (1 = '6.8' 2 = '4/10' 3 = '2/12'): ";
+    cout << "Enter type of bet (1 = '2/12' 2 = '4/10' 3 = '6/8' 4 = '3/9'): ";
     cin >> BetType;
 
-    if ((BetType == 1) || (BetType == 2) || (BetType == 3)) {
+    if ((BetType == 1) || (BetType == 2) || (BetType == 3) || (BetType == 4)) {
         return BetType;
     }
     else {
-        return 1;
+        cout << "Please enter a valid number.";
+        return 0;
     }
 }
 
@@ -110,20 +132,12 @@ short DoDiceThrow(void) {
     short DiceValue;
 
     srand(time(NULL));
-    DiceValue = (rand() % 11) + 2;
+    DiceValue = (rand() % 12) + 1;
 
-    if ((DiceValue == 4) || (DiceValue == 10)) {
+    if ((DiceValue == 4) || (DiceValue == 10) || (DiceValue == 2) || (DiceValue == 12)
+       ||(DiceValue == 6) || (DiceValue == 8) || (DiceValue == 3) || (DiceValue == 9)) {
         srand(time(NULL));
         DiceValue = (rand() % 12) + 1;
-    }
-    if ((DiceValue == 2) || (DiceValue == 12)) {
-        srand(time(NULL));
-        DiceValue = (rand() % 12) + 1;
-
-    if ((DiceValue == 2) || (DiceValue == 12)) {
-            srand(time(NULL));
-            DiceValue = (rand() % 12) + 1;
-     }
     }
 
     return DiceValue;
@@ -136,22 +150,29 @@ unsigned short DoMoneyCalc(short Dice, short Bet, short BetMoney) {
     switch (Bet) {
 
     case 1:
-        if ((Dice == 6) || (Dice == 8)) {
-            MoneyEarned = BetMoney * 1.5;
+        if ((Dice == 2) || (Dice == 12)) {
+            MoneyEarned = BetMoney * 5;
         }
         break;
 
     case 2:
-        if ((Dice == 10) || (Dice == 4)) {
+        if ((Dice == 4) || (Dice == 10)) {
             MoneyEarned = BetMoney * 2.5;
         }
         break;
 
     case 3:
-        if ((Dice == 2) || (Dice == 12)) {
-            MoneyEarned = BetMoney * 5;
+        if ((Dice == 6) || (Dice == 8)) {
+            MoneyEarned = BetMoney * 1.5;
         }
         break;
+
+    case 4:
+        if ((Dice == 3) || (Dice == 9)) {
+            MoneyEarned = BetMoney * 1.7;
+        }
+        break;
+
     default:
         MoneyEarned = 0;
         break;
