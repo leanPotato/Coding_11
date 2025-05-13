f258fc5629587dc395e2ba16c5b32494e1f02981
Feli <quantumleap1528@gmail.com>
2025-05-13 15:08:53 -0700

    Add files via upload

--- Projects/CS11_Phone_Book/Phone_Book.cpp
+++ Projects/CS11_Phone_Book/Phone_Book.cpp
@@ -12,11 +12,11 @@
 
 using namespace std;
 
-int lookupName(string targetName, string names[], string phoneNumbers[], int 7);
+string lookupName(string targetName, string names[], string phoneNumbers[], int size);
 
 int main() {
 
-    string names[]        = { "Micheal Myers", "Ash Williams", "Jack Torrance", "Freddy Krueger" "Albert Einstein" , "Issac Newton" , "Steve Jobs" };
+    string names[]        = { "Micheal Myers", "Ash Williams", "Jack Torrance", "Freddy Krueger", "Albert Einstein" , "Issac Newton" , "Steve Jobs" };
 
     string phoneNumbers[] = { "333-8000" , "333-2323", "333-6150", "339-7970", "888 8888", "999 9999", "777 7777" };
 
@@ -25,33 +25,36 @@ int main() {
     
     do {
 
-        cout << "Enter a name to find the corresponding phone number." << endl;
+        cout << "Enter a name to find the corresponding phone number: ";
 
         getline(cin, targetName);
         targetPhone = lookupName(targetName, names, phoneNumbers, 7);
 
         if (targetPhone.length() > 0) {
 
-            cout << "The number is: " << targetPhone << endl;
+            cout << "\nThe number is: " << targetPhone << endl << endl;
 
-        }
-        else
+        } else {
 
             cout << "Name not found. " << endl;
+            cout << "look up another name? (y/n)" << endl;
 
+            cin >> c;
+            cin.ignore();
         }
-
-        cout << "look up another name? (y/n)" << endl;
-
-        cin >> c;
-        cin.ignore();
-
     } while (c == 'y');
 
     return 0;
 }
 
-int lookupName(string targetName, string names[], string phoneNumbers[], int 7) {
+string lookupName(string targetName, string names[], string phoneNumbers[], int size) {
 
-    
+    for (int counter = 0; counter < size; ++counter) {
+
+        if (names[counter] == targetName) {
+
+            return phoneNumbers[counter];
+        }
+    }
+    return "";
 }
