7182c10f4b31a517e6f6b26529e7939277494263
Felipiano15$ <quantumleap1528@gmail.com>
2025-06-18 23:08:12 -0700

    Final forever

--- Projects/CS11_Airplane_Seats/CS11_Airplane_Seats.cpp
+++ Projects/CS11_Airplane_Seats/CS11_Airplane_Seats.cpp
@@ -13,17 +13,17 @@
 
 using namespace std;
 
-void showSeating (char seats[][4], int rows, int colmn);
-void addPassenger(char seats[][4], int rows, int colmn);
-bool isFull(char seats[][4], int rows, int colmn);
+void showSeating (char seats[][6], int rows, int colmn);
+void addPassenger(char seats[][6], int rows, int colmn);
+bool isFull(char seats[][6], int rows, int colmn);
 void showIntroScreen();
 
 int main() {
 
     char cmd;
     string input;
-    const int  NUM_ROWS = 7;
-    const int  NUM_SEATS = 4;
+    const int  NUM_ROWS = 9;
+    const int  NUM_SEATS = 6;
     
     char seats[NUM_ROWS][NUM_SEATS];
     char Quit = 'q';
@@ -32,10 +32,10 @@ int main() {
 
     showIntroScreen();
 
-    for (int r = 0; r < NUM_ROWS; ++r) {
+    for (int row = 0; row < NUM_ROWS; ++row) {
 
-        for (int c = 0; c < NUM_SEATS; ++c) {
-            seats[r][c] = 'A' + c;
+        for (int col = 0; col < NUM_SEATS; ++col) {
+            seats[row][col] = 'A' + col;  //  Fills seat letters
         }
     }
 
@@ -45,8 +45,9 @@ int main() {
         cin >> input;  // Recieves the input the user puts in
 
 
-        if (input.length() != 1 || (input != "a" && input != "s" && input != "q")) {
+        if (input != "a" && input != "s" && input != "q") {
 
+            input.clear();  // clears the string
             cout << "This input is invalid. Please only input a, s, or q.\n";
             continue;
         
@@ -88,22 +89,65 @@ void showIntroScreen() {
 
 }
 
-void showSeating(char seats[][4], int rows, int colmn) {
+void showSeating(char seats[][6], int rows, int colmn) {
 
     cout << "\nShowing seating chart (0 = Open seat, X = Taken seat):\n\n";
     cout << "";
 
+
+    cout << "           _      "   << endl;
+    cout << "          / \\    "   << endl;
+    cout << "         /   \\   "   << endl;
+    cout << "        /     \\  "   << endl;
+    cout << "       /       \\ "   << endl;
+    cout << "      /         \\ "  << endl;
+    cout << "     /           \\ " << endl;
+    cout << "    |             | " << endl;
+
     for (int r = 0; r < rows; ++r) {
-        cout << r + 1 << ' ' << ' ';
+
+        if (r == 3 || r == 8) {
+
+            cout << "    |             |" << endl;
+
+        }
+
+        cout << r + 1 << "  ";
+
+        if (r < 9) {
+
+            cout << " ";
+
+        }
+
         for (int c = 0; c < colmn; ++c) {
+
+
+
+            if (c == 0) {
+
+                cout << "|";
+
+            } else if (c == 3) {
+
+                cout << "  ";
+ 
+            }
+
             cout << seats[r][c] << ' ';
+            
+            if (c == 5) {
+
+                cout << "\b|";
+                
+            }
         }
         cout << endl;
     }
 }
 
 
-void addPassenger(char seats[][4], int rows, int colmn) {
+void addPassenger(char seats[][6], int rows, int colmn) {
 
     string input;
     cout << "Type the seat you would like and we will check if its available! (e.g. : 1A, 2B): ";
@@ -114,36 +158,38 @@ void addPassenger(char seats[][4], int rows, int colmn) {
 
     if (input.length() == 2 && (input[0] >= '0') && (input[0] <= '9')) {
 
-        row = input[0] - '0';
+        row = input[0] - '1';  // Taking row from input
         seatLet = input[1];
-    }
-    else {
+
+    } else {
 
         cout << "Input is invalid please try again.\n";
         return;
+
     }
 
     if (row < 1 || row > rows || seatLet < 'A' || seatLet >= 'A' + colmn) {
 
         cout << "Input is invalid please try again.\n";
         return;
+
     }
 
-    int rowNum = row - 1;
-    int seatNum = seatLet - 'A';
+    int seatNum = seatLet - 'A';  // Find col 
 
-    if (seats[rowNum][seatNum] == 'X') {
+    if (seats[row][seatNum] == 'X') {
 
         cout << "Seat is already taken";
-    }
-    else {
 
-        seats[rowNum][seatNum] = 'X';
+    } else {
+
+        seats[row][seatNum] = 'X';
         cout << "Seat " << input << " has been assigned to you.\n";
+
     }
 }
 
-bool isFull(char seats[][4], int rows, int colmn) {
+bool isFull(char seats[][6], int rows, int colmn) {
 
     for (int r = 0; r < rows; ++r)
         for (int c = 0; c < colmn; ++c)
